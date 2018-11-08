c9ad49d007388864d2d4a4e6122e82136dda870f
Liran Nuna <liranuna@gmail.com>
2018-11-07 22:26:28 -0800

    Moved config to their own module

---
+++ sintia/config.py
@@ -0,0 +1,18 @@
+from configparser import ConfigParser
+from configparser import SectionProxy
+
+from sintia.util import memoize
+
+
+@memoize
+def get_config():
+    config = ConfigParser()
+    config.read('config.ini')
+
+    return config
+
+
+@memoize
+def get_config_section(section_name: str) -> SectionProxy:
+    config = get_config()
+    return config[section_name]
--- start.py
+++ start.py
@@ -1,11 +1,6 @@
-from configparser import ConfigParser
-
 from sintia.sintia import Sintia
 
 
 if __name__ == '__main__':
-    config = ConfigParser()
-    config.read('config.ini')
-
-    sintia = Sintia(config)
+    sintia = Sintia()
     sintia.run()
