7c075941602f99bb67e6af492455d3f20c126f5b
Liran Nuna <liranuna@gmail.com>
2019-02-24 15:10:23 -0800

    Fixed a bug with memoize decorator

--- sintia/util.py
+++ sintia/util.py
@@ -1,16 +1,13 @@
 import asyncio
 import inspect
-from itertools import tee
 from functools import lru_cache
 
 
 def memoize(user_function):
     def interceptor(*args, **kwargs):
         result = user_function(*args, **kwargs)
-        if asyncio.iscoroutine(result):
+        if inspect.isawaitable(result):
             return asyncio.ensure_future(result)
-        if inspect.isgenerator(result):
-            return tee(result)
 
         return result
 
